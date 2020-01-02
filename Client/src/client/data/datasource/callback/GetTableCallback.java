37dd029b0cf9d1432c010a1220d0caca5c67da78
Zojae031 <edgar3@naver.com>
2020-01-02 22:50:28 +0900

    Separate Interface

--- Client/src/client/data/datasource/callback/GetTableCallback.java
+++
@@ -1,9 +0,0 @@
-package client.data.datasource.callback;
-
-import client.data.dao.IngredientModel;
-
-import java.util.Vector;
-
-public interface GetTableCallback {
-    void IgSuccess(Vector<IngredientModel> lists);
-}
