f2b0776c5477d88a2bfeb1e8b6211fcb186d2e9a
Zojae031 <edgar3@naver.com>
2020-01-03 17:18:54 +0900

    Add ServerConnection Callback comment

--- Client/src/client/data/datasource/callback/ServerConnectionCallback.java
+++ Client/src/client/data/datasource/callback/ServerConnectionCallback.java
@@ -5,6 +5,15 @@ import client.data.dao.ProductModel;
 
 import java.util.Vector;
 
+/**
+ * 서버와 통신시 필요한 Callbcak interface
+ *
+ * @author 조재영
+ * @see ServerConnectionCallback 연결시 필요한 base 콜백
+ * @see GetTableCallback 재료목록을 얻어올 때 필요한 콜백
+ * @see SelectItemCallback 선택된 아이템을 얻어올 때 필요한 콜백
+ * @see TotalMoneyCallback 총 구입 금액을 얻어올 때 필요한 콜백
+ */
 public interface ServerConnectionCallback {
     void accept(Vector<ProductModel> lists);
 
